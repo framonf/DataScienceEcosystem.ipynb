391623509ae81aa106a71412d4c5ace49e3c15eb
framonf <135824008+framonf@users.noreply.github.com>
2023-06-08 12:44:41 +0200

    Add files via upload

---
+++ Data Science Ecosystem in Jupiter Notebook.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## H2: Data Science Tools and Ecosystem
+
+# ### *_In this notebook, Data Science Tools and Ecosystem are summarized_*
+
+# #### Some of the popular languages that Data Scientists use are:
+#     
+#     -Julia
+#     
+#     -Python
+#     
+#     -R
+
+# #### Some of the commonly used libraries used by Data Scientists include:
+# 
+#     - GadFly.jl (Julia)
+#     
+#     - Google Charts (Julia)
+#     
+#     - Makie.jl (Julia)
+#     
+#     - Plots.jl (Julia)
+#     
+#     - PyPlot.jl (Julia)
+#     
+#     - Numpy (Python)
+#     
+#     - Pandas (Python)
+#     
+#     - Matplotlib (Python)
+#     
+#     - Scikit-learn (Python)
+#     
+#     - TensorFlow (Python)
+#     
+#     - Keras (Python)
+#     
+#     - Dplyr (R)
+#     
+#     - Ggplot2 (R)
+#     
+#     - Caret (R)
+#     
+#     - Prophet (R)
+#     
+#     - Stringr (R)
+#     
+#     
+#     
+#    
+
+# | Data Science Tools |
+# |--------------------|
+# | R Studio |  
+# | Apache Spark |  
+# | Apache Hadoop | 
+# 
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+
+# In[1]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+
+(3*4)+5 
+
+
+# In[2]:
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+200/60
+
+
+# ### Objectives of the Course in Data Science
+# 
+#     * Reputable Certificate from 500 Fortune company IBM
+#     * Start learning Programming languages like Python, R, and SQL
+#     * Apply data analysis techniques to real world problems
+#     * Communicate results using data visualization techniques.
+
+# ##  Author 
+# ### Francesco Monforte
+#     
+
+# In[ ]:
+
+
+
+
